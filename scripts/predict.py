4006cd1e19a8d36c0cb199c8a21de49044191b20
Jia Runchang <109291055+Jrunchang@users.noreply.github.com>
2023-11-01 12:23:52 +0800

    Add files via upload

--- scripts/predict.py
+++ scripts/predict.py
@@ -20,6 +20,10 @@ import scipy.sparse as sp
 import torch.nn as nn
 from biotransformers import BioTransformers
 from Bio import SeqIO
+from Bio.PDB import PDBParser
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqUtils import seq3
 
 
 # Path
@@ -127,7 +131,27 @@ def test(test_dataframe):
         # with open(model_name.split(".")[0] + "_pred.pkl", "wb") as f:
             # pickle.dump(pred_dict, f)
     return  test_pred, dict
+def parse_pdb_to_fasta(pdb_file):
+  
+    three_to_one_letter = {"ALA": "A","ARG": "R","ASN": "N","ASP": "D","CYS": "C","GLN": "Q","GLU": "E","GLY": "G","HIS": "H", "ILE": "I",
+                           "LEU": "L","LYS": "K","MET": "M","PHE": "F","PRO": "P","SER": "S","THR": "T", "TRP": "W","TYR": "Y","VAL": "V"}
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure("protein", pdb_file)
 
+    protein_seq = ""
+    for model in structure:
+        for chain in model:
+            for residue in chain:
+                if residue.get_id()[0] == ' ':
+                    three_letter_code = residue.get_resname()
+                    one_letter_code = three_to_one_letter.get(three_letter_code, "X")
+                    protein_seq += one_letter_code
+
+    # create a Seq object.
+    protein_seq = Seq(protein_seq)
+    protein_seq =str(protein_seq)
+
+    return protein_seq
 
 
 def process_fasta(fasta1,site):
@@ -152,17 +176,48 @@ def process_fasta(fasta1,site):
 
     return idList,seq15list
 
+
+def process_another(seq,site):
+ 
+    seq15list = []
+    site = site.split(',')
+    for si in (site):
+        si = int(si)
+        start_pos = max(1, si - 7)
+        end_pos = min(len(seq), si + 7)
+        center_sequence = seq[start_pos-1:end_pos]
+        if len(center_sequence) < 15:
+            add_aa = 'A' * (15 - len(center_sequence))
+            center_sequence = center_sequence + add_aa
+        seq15list.append(center_sequence)
+        
+
+
+    return seq15list
+
 def main():
     dict_res={}
     parser = argparse.ArgumentParser()
-    parser.add_argument("--fasta", type = str, help = "Input fasta file")
+    parser.add_argument("--input", type = str, help = "Input file (PDB or FASTA)")
     parser.add_argument("--site", type = str, help = "metal binding site")
     parser.add_argument("--outpath", type = str, help = "Output path to save disease-associated predictions")
     args = parser.parse_args()
-    # outpath = args.outpath.rstrip("/") + "/"
+    idList,seq15list = [],[]
+    if args.input.endswith(".pdb"):
+        pdb_file = args.input
+        id = pdb_file.split('.')[0]
+        seqold = parse_pdb_to_fasta(pdb_file)
+        seq15list = process_another(seqold,args.site)
+        for i in range(len(seq15list)):
+            idList.append(id)
+
+    elif args.input.endswith(".fasta"):
+        idList,seq15list = process_fasta(args.input,args.site)
+
+    else:
+        print("Please input the PDB or FASTA format")
+    #################################################################################################################################################
     AAs = []
-    idList,seq15list = process_fasta(args.fasta,args.site)
-    # if len!
     outp = args.outpath
     print(outp)
     positions = args.site.split(',')
@@ -170,7 +225,7 @@ def main():
         AA = i[7]
         AAs.append(AA)
     test_dic = {"ID": idList, "sequence": seq15list,"embedd":[]}
-
+    print(test_dic)
     bio_trans = BioTransformers(backend="esm1_t6_43M_UR50S")
     embedds = []
     for seq1 in seq15list:
@@ -181,11 +236,12 @@ def main():
         embedds.append(embedding)
 
     test_dic['embedd'] = embedds
-    # print(test_dic)
+    # print(embedds[0].shape)
 
     test_dataframe = pd.DataFrame(test_dic)
-    # print(test_dataframe)
+    print(test_dataframe)
     test_pred2,dict1=test(test_dataframe)
+    print(test_pred2)
     result = []
     for i in test_pred2:
         if float(i)>=0.5:
@@ -194,7 +250,7 @@ def main():
             result.append('No')
 
     dict_res = {"Rank":[i+1 for i in range(len(AAs))],"Uniprot ID":idList,"AA":AAs,"position":positions,"Probability":test_pred2,"Prediction Result":result}
-    print(dict_res)
+    # print(dict_res)
     df = pd.DataFrame(dict_res)
     df.to_csv(outp,index=False)
 
